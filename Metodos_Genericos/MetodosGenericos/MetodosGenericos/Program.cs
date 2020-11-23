8e596630176b17e247673acc71f10558bc1ddc12
Git da Bru <bruna.reveriego91@gmail.com>
2020-11-23 20:30:58 -0300

    metodos genericos

---
+++ Metodos_Genericos/MetodosGenericos/MetodosGenericos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosGenericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Trocar<int>(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+ 
+            string x = "primeiro";
+            string y = "segundo";
+
+            Trocar<string>(ref x, ref y);
+            Console.WriteLine(x + " " + y);
+            Console.ReadKey();
+        }
+
+        private static void Trocar<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+}
