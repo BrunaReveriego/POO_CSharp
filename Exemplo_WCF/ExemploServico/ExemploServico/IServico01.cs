12f12f77b433e769a9f29ead1e7eb19c838d89a2
Git da Bru <bruna.reveriego91@gmail.com>
2021-05-30 18:10:57 -0300

    exemplo WCF

---
+++ Exemplo_WCF/ExemploServico/ExemploServico/IServico01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ExemploServico
+{
+
+    //Metadados da interface
+    [ServiceContract]
+    public interface IServico01
+    {
+        //coisas que serviço faz
+        [OperationContract]
+        string Mensagem();
+
+        //coisas que serviço faz
+        [OperationContract]
+        int Somar(int x, int y);
+
+
+    }
+}
---
+++ Exemplo_WCF/ExemploServico/ExemploServico/Servico01.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ExemploServico
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Servico01" no arquivo de código, svc e configuração ao mesmo tempo.
+    // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione Servico01.svc ou Servico01.svc.cs no Gerenciador de Soluções e inicie a depuração.
+    public class Servico01 : IServico01
+    {
+        public string Mensagem()
+        {
+            return "Isso vem de um servidor usando WCF!";
+        }
+
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
