ceee5e60479f14f2bd9329a0285ea8292325049e
Git da Bru <bruna.reveriego91@gmail.com>
2021-05-31 11:43:33 -0300

    exemplo cliente usando rest

---
+++ ExemploServicoRest/ClienteRestWindows/Fornecedor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClienteRestWindows
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
